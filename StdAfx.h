2b81901deb8f935dd608476528ff21b5720ddfa4
Canada, Masakatz <canadie@gmail.com>
2009-12-07 18:00:22 +0900

    Fix Japanese

--- StdAfx.h
+++ StdAfx.h
@@ -1,7 +1,4 @@
-// stdafx.h : 標準のシステム インクルード ファイル、
-//            または参照回数が多く、かつあまり変更されない
-//            プロジェクト専用のインクルード ファイルを記述します。
-//
+
 
 #if !defined(AFX_STDAFX_H__00C71AB2_5A86_4B79_979D_E4BC2E2C1CF4__INCLUDED_)
 #define AFX_STDAFX_H__00C71AB2_5A86_4B79_979D_E4BC2E2C1CF4__INCLUDED_
@@ -10,15 +7,13 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define WIN32_LEAN_AND_MEAN // Windows ヘッダーから殆ど使用されないスタッフを除外します
+#define WIN32_LEAN_AND_MEAN 
 
 #include <stdio.h>
 #include <afx.h>
-#include <afxwin.h>         // MFC のコアおよび標準コンポーネント
+#include <afxwin.h> 
 
-// TODO: プログラムで必要なヘッダー参照を追加してください。
 
 //{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ は前行の直前に追加の宣言を挿入します。
 
 #endif // !defined(AFX_STDAFX_H__00C71AB2_5A86_4B79_979D_E4BC2E2C1CF4__INCLUDED_)
